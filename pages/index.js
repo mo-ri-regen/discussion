33250caf119d76a68bd14174ca5aa77a55a16061
mo-ri-regen <moricontb@gmail.com>
2021-05-01 22:05:03 +0900

    🎉 begin pannel discussion project

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,74 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { useState } from "react";
 
 export default function Home() {
+  //投稿テキスト
+  const [contributionText, setContributionText] = useState("");
+  //話し終えた話題
+  const [finishedText, setFinishedText] = useState("");
+  //お話し中
+  const [talkingText, setTalkingText] = useState("");
+  //未着手の話題
+  const [noBeginingText, setNoBeginingText] = useState("");
+
+  //未着手の話題へいくテキスト
+  const onChangeText = (e) => {
+    setContributionText(e.target.value);
+    //setAddText(e.target.value);
+  };
+  const onClickAdd = () => {
+    alert(text);
+  };
+  //投稿ボタンクリック
+  const handleContribution = (e) => {
+    // setContributionText(contributionText);
+    const text = [...noBeginingText, contributionText];
+    setNoBeginingText(text);
+    setContributionText("");
+  };
+
+  // constructor(props){
+  //   super(props);
+
+  //}
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>パネルディスカッション</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <h1 className={styles.title}>Pannel Discussion</h1>
+        <div className="no-begining-area">
+          {/* <textarea value={noBeginingText} onChange={onChangeText}></textarea> */}
+          {/* {noBeginingText.map((t) => {
+            return t;
+          })} */}
+          {[...noBeginingText]}
+        </div>
+        <div className>
+          <label for="contribute">投稿</label>
+          <textarea
+            id="contribute"
+            value={contributionText}
+            onChange={onChangeText}
+          ></textarea>
+          <button onClick={handleContribution}>投稿</button>
         </div>
       </main>
 
-      <footer className={styles.footer}>
+      {/* <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
-      </footer>
+      </footer> */}
     </div>
-  )
+  );
 }

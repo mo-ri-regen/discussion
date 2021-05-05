b83b546bb346f8203122384412186ba4cee21ed8
mo-ri-regen <moricontb@gmail.com>
2021-05-05 17:12:46 +0900

    💄 add Header

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -1,10 +1,21 @@
 import '/'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 import Link from 'next/link'
+import { Header } from '../components/header'
 
 export default function Admin({ Component, pageProps }) {
+  
   return (
-    <div>
-      <p>aaa</p>
+    <div className={styles.container}>
+      <Head>
+        <title>パネルディスカッション</title>
+        <link rel="icon" href="/it-kingdom.png" />
+      </Head>
+
+      <Header />
+
+      <main className={styles.main}></main>
     </div>
     // return <Component {...pageProps} />
   );
